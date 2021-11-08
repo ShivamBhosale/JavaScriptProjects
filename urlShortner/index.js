d1142ab928ff5228b96294223d87705d05a1ab15
Shivam Bhosale <50315818+ShivamBhosale@users.noreply.github.com>
2021-11-08 13:39:29 +0530

    commit 2

--- urlShortner/index.js
+++ urlShortner/index.js
@@ -1,20 +1,25 @@
 const shortBtn = document.getElementById('shorten-btn');
 const outputLink = document.getElementById('output');
+
 const urlInput = document.getElementById('url-input');
 // https://api.shrtco.de/v2/shorten?url=shivambhosale.netlify.app
-const url = 'https://api.shrtco.de/v2/shorten?url=' + urlInput.value;
-console.log(url);
+
+
 shortBtn.addEventListener('click',function(){
     
+    const url = 'https://api.shrtco.de/v2/shorten?url=' + urlInput.value;
+    console.log(url);
     fetch(url)
     .then(response => response.json())
     .then(data => {
         const shortURL = data.result.short_link;
-        outputLink.textContent = shortURL;
         
+        outputLink.textContent = shortURL;
+        outputLink.href =  urlInput.value;
         // outputLink.innerHTML = `<a target="_blank" href=${shortURL}>${shortURL}</a>`
         // console.log(outputLink.href);
         console.log(shortURL)
+        console.log(urlInput.value);
         
     })
    
